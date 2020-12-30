e13628d63862c41a3d23bf9929d1d42fb9613cdc
Mohamed <malbgal@gmail.com>
2020-12-29 20:56:54 -0800

    cdm

--- solutions/data_structures/ArraySolutions.java
+++ solutions/data_structures/ArraySolutions.java
@@ -5,31 +5,28 @@ import java.util.Arrays;
 public class ArraySolutions {
 
     // # 1
-    public static int maxIncreaseKeepingSkyline(final int[][] grid){
+    public static int maxIncreaseKeepingSkyline(int[][] grid){
 
-    //get max value of each row
-        final int[] rowMaxes = new int[grid.length];
+        //get max value of each row
+        int[] rowMaxes = new int[grid.length];
         int r_i = 0;
-        for (final int[] elt : grid){
+        for (int[] elt : grid){
             rowMaxes[r_i++] = Arrays.stream(elt).max().getAsInt();
         }
-
-        //get max value of each column
-        final int[] colMaxes = new int[grid.length];
-
+        
+         int[] colMaxes = new int[grid.length];
+         int z = 0;
         //transpose the input array
-        final int[][] transpose = new int[grid.length][grid[0].length];
+        int[][] transpose = new int[grid.length][grid[0].length];
         for (int i = 0; i < grid.length; i++){
             for (int j =0; j < grid.length; j++){
                 transpose[j][i] = grid[i][j];
             }
         }
-        int z = 0;
-        for (final int[] elt: transpose){
+        //get max value of each column
+        for ( int[] elt: transpose){
             colMaxes[z++] = Arrays.stream(elt).max().getAsInt();
         }
-
-
         int diffs = 0;
         for (int ii = 0; ii < grid.length; ii++){
 
