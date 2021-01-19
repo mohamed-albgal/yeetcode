01c34686d08d41ea924f25d7d0fe13e9889dfcd7
Mohamed <malbgal@gmail.com>
2021-01-18 19:23:16 -0800

    added sub array sort

--- README.md
+++ README.md
@@ -47,6 +47,7 @@
 1|2d Array - Keep Skyline | Medium |[leetcode](https://leetcode.com/problems/max-increase-to-keep-city-skyline/submissions/) |[:unicorn:](solutions/data_structures/ArraySolutions.java)
 2 |Merge Intervals | medium |https://leetcode.com/problems/merge-intervals/ |[:unicorn:](solutions/data_structures/ArraySolutions.java)
 3 |Word Search | medium |https://leetcode.com/problems/word-search/ |[:unicorn:](solutions/data_structures/ArraySolutions.java)
+4 | Sub Array Sort | medium | | [:unicorn:](solutions/data_structures/ArraySolutions.java)
 
 
 ## BINARAY SEARCH
--- solutions/data_structures/ArraySolutions.java
+++ solutions/data_structures/ArraySolutions.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 
 public class ArraySolutions {
 
-    // # 1
+// # 1
     public static int maxIncreaseKeepingSkyline(int[][] grid){
 
         //get max value of each row
@@ -42,7 +42,7 @@ public class ArraySolutions {
 
 
     }
-    //2
+//#2
     public int[][] merge(int[][] intervals) {
         if(intervals.length <= 1) {
             return intervals;
@@ -70,8 +70,7 @@ public class ArraySolutions {
         }
         return result.toArray(new int[result.size()][2]);
     }
-
-    //3
+//#3
         public boolean exist(char[][] board, String word) {
         Map<Character,HashMap<Character,Integer>> adj = new HashMap<>();
         int count = 0;
@@ -115,4 +114,35 @@ public class ArraySolutions {
         }
         return false;
     }
+
+// #4
+    /*
+        given a partially sorted array
+        find the start and end indices of the sub array that is out of order or -1 -1 if sorted already
+        [1 2 3 10 3 4 2 13]  returns [2, 6]
+    */
+      public static int[] subarraySort(int[] array) {
+        Map<Integer, Integer> map = new HashMap<>();
+        //store original index of each value in the array
+        for (int i = 0; i < array.length; i++)
+            map.put(i, array[i]);
+        //sort the array
+        Arrays.sort(array);
+        //store the new index of each value
+        Map<Integer, Integer> sorted = new HashMap<>();
+        for (int i = 0; i < array.length; i++)
+            sorted.put(i, array[i]);
+
+        //compare different mappings, the minimum and most maximum differences are the solution
+        Iterator<Entry<Integer, Integer>> iter = sorted.entrySet().iterator();
+        //filter out any non-identical mapping entries and store
+        List<Integer> diffs = map.entrySet().stream().
+            filter(e -> !e.equals(iter.next())).
+            map(e -> e.getKey()).
+            collect(Collectors.toList());
+
+
+        //first and last in the collection are returned
+        return diffs.size() == 0 ? new int[]{-1,-1}: new int[]{diffs.get(0),diffs.get(diffs.size()-1)};
+  }
 }
